34eec0ab8bd49c92194396a4a7a4005d537a4f50
Takuma Saito <takuma_saito@TakumanoMacBook-Pro.local>
2017-06-01 17:37:29 +0900

    Add character data

---
+++ Assets/mentor_collection/Scripts/MasterDataManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterDataManager : MonoBehaviour
+{
+	[SerializeField]
+	private List<MstCharacter> characterTable = new List<MstCharacter>();
+
+	private void Start()
+	{
+		var characterCSV = Resources.Load("CSV/Character.csv") as TextAsset;
+		var csv = CSVReader.SplitCsvGrid(characterCSV.text);
+		for (int i=1; i<csv.GetLength(1)-1; i++) 
+		{
+			var data = new MstCharacter();
+			data.SetFromCSV( GetRaw(csv, i) );
+			characterTable.Add(data);
+		}
+	}
+
+	private string[] GetRaw (string[,] csv, int row) {
+		string[] data = new string[ csv.GetLength(0) ];
+		for (int i=0; i<csv.GetLength(0); i++) {
+			data[i] = csv[i, row];
+		}
+		return data;
+	}
+}
---
+++ Assets/mentor_collection/Scripts/MstCharacter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[System.SerializableAttribute]
+public class MstCharacter
+{
+	[SerializeField]
+	private int
+		id,
+		rarity,
+		maxLebel,
+		growthType,
+		lowerEnergy,
+		upperEnergy,
+		initialCost;
+
+	[SerializeField]
+	private string
+		name,
+		imageId,
+		flavorText;
+
+	public void SetFromCSV(string[] data)
+	{
+		id          = int.Parse( data[0] );
+		name        = data[1];
+		imageId     = data[2];
+		flavorText  = data[3];
+		rarity      = int.Parse( data[4] );
+		maxLebel    = int.Parse( data[5] );
+		growthType  = int.Parse( data[6] );
+		lowerEnergy = int.Parse( data[7] );
+		upperEnergy = int.Parse( data[8] );
+		initialCost = int.Parse( data[9] );
+	}
+}
